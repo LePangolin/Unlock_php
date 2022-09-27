0a9eb50b6aa748d1e1c4da2d078af715c5ceae59
LePangolin <91209500+LePangolin@users.noreply.github.com>
2022-09-27 11:40:42 +0200

    feat(View): ajout de la structure twig

--- public/index.php
+++ public/index.php
@@ -3,14 +3,24 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$twig = Twig::create(__DIR__ . '/Views', ['cache' => false]);
+
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.html.twig', [
+        'title' => 'Accueil',
+        'content' => 'Incroyabel ça marche'
+    ]);
+})->setName('Index');
 
 $app->run();
