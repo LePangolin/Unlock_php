c26e85ef6b3a57af8ecbefbc33aa6e92a6e3481f
LePangolin <91209500+LePangolin@users.noreply.github.com>
2022-10-27 11:16:04 +0200

    fix(Chargement Partie): ajout logger

--- app/Services/CardService.php
+++ app/Services/CardService.php
@@ -19,18 +19,21 @@ final class CardService
     public function getCard($id)
     {
         $card = $this->em->getRepository(Card::class)->findOneBy(['id' => $id]);
+        $this->logger->info("Card {$id} found");
         return $card;
     }
 
     public function getCards()
     {
         $cards = $this->em->getRepository(Card::class)->findAll();
+        $this->logger->info("Cards found");
         return $cards;
     }
 
     public function getCardByDeckId($deckId)
     {
         $cards = $this->em->getRepository(Card::class)->findBy(['deck_id' => $deckId]);
+        $this->logger->info("Cards found for deck {$deckId}");
         return $cards;
     }
 }
